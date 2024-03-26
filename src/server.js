6006ca245b91554579b4e307af58165d334b9bc7
JChatmasOctopus <50506513+JChatmasOctopus@users.noreply.github.com>
2024-03-26 13:31:41 -0500

    User story #69823
    Update constants.

--- src/server.js
+++ src/server.js
@@ -3,7 +3,9 @@
 const express = require('express');
 
 // Constants
+//const PORT = process.env.PORT || '8080';
 const PORT = process.env.PORT || '8081';
+//const HOST = '192.168.0.1';
 const HOST = '0.0.0.0';
 
 // App
